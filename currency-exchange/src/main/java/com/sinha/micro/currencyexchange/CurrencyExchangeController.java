24a3bfa38865721bf39485ed59bed919cac1fa8d
Aakarsh Sinha <aakarshsak@gmail.com>
2023-10-25 01:20:40 +0530

    Working after docker compose

--- currency-exchange/src/main/java/com/sinha/micro/currencyexchange/CurrencyExchangeController.java
+++ currency-exchange/src/main/java/com/sinha/micro/currencyexchange/CurrencyExchangeController.java
@@ -4,6 +4,7 @@ package com.sinha.micro.currencyexchange;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/currency-exchange")
 public class CurrencyExchangeController {
+
+    @Value("${spring.datasource.url}")
+    private String url;
 
     Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 
@@ -26,7 +30,7 @@ public class CurrencyExchangeController {
 
     @GetMapping("/from/{from}/to/{to}")
     public ResponseEntity<CurrencyExchange> getCurrency(@PathVariable Currency from, @PathVariable Currency to) {
-        logger.info("My info");
+        logger.info("My info: " + url);
         return new ResponseEntity<>(currencyExchangeService.getCurrencyExchangeFromAndTo(from, to), HttpStatus.OK);
     }
 }
